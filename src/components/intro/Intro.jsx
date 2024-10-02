2c4e02736bda097d1ce36dfce755d5c1707f39a1
vishal019 <vishalrkjeur019@gmail.com>
2024-10-02 12:17:33 +0530

    commit

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -12,13 +12,13 @@ const Intro = (props) => {
     <section id='intro'>
 
     <div className="inroContent">
-        <span className="hello">Hello,</span>
-        <span className="introtext">We Provide Exclusive
+        <span className="hello">Hello I am Vishal Kamble ,</span>
+        <span className="introtext">I Provide Exclusive
  <br /> Web & App Development Services</span>
         <p className='intropara'>We are skilled and passionate web developer with experience in creating user friendly websites & Mobile Applications. </p>
     <Link> <button className='btn'><span class="material-symbols-outlined">
 cases
-</span>Learn More</button></Link>    </div>
+</span>Get Started</button></Link>    </div>
   
   <figure>  <img src={profile} alt=""  className='bg'/></figure>
     </section>
